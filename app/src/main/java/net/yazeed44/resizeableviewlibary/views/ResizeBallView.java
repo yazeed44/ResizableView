b51656eabab6715ed12791b646dd5d4d53f343be
yazeed44 <g--56@hotmail.com>
2014-10-18 22:02:23 +0300

    Changed balls to be a view instead of just drawing
    started working on the new mechanism , it's hard but worth it

---
+++ app/src/main/java/net/yazeed44/resizeableviewlibary/views/ResizeBallView.java
@@ -0,0 +1,54 @@
+package net.yazeed44.resizeableviewlibary.views;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import net.yazeed44.resizeableviewlibary.R;
+
+
+/**
+ * Created by yazeed44 on 10/18/14.
+ */
+public class ResizeBallView extends ImageView {
+
+    public static int count = 0;
+    private int id;
+
+    private String className = getClass().getSimpleName() + "   ";
+
+    public ResizeBallView(final Context context) {
+        super(context);
+        initializeCircle();
+        initializeId();
+
+    }
+
+
+    private void initializeId() {
+        if (count > 4) {
+            throw new IllegalStateException("There's should be four balls only !!" + "\n" + id);
+        }
+
+        this.id = count++;
+    }
+
+
+    private void initializeCircle() {
+        final Drawable resizeBallDrawable = getResources().getDrawable(R.drawable.resize_circle);
+
+        final Bitmap resizeBall = ((BitmapDrawable) resizeBallDrawable).getBitmap();
+
+        setImageBitmap(resizeBall);
+
+
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+
+}
